6cbd615f2ec15fccf819936e210cea989c0f7622
Petar Jagrić <83279909+petarjagric@users.noreply.github.com>
2021-05-22 22:49:52 +0200

    alter tables

--- kolokvij_vjezba2.sql
+++ kolokvij_vjezba2.sql
@@ -71,3 +71,11 @@ create table svekar(
 );
 
 
+alter table prijatelj add foreign key (svekar) references svekar(sifra);
+alter table brat add foreign key (neprijatelj) references neprijatelj(sifra);
+alter table neprijatelj add foreign key (cura) references cura(sifra);
+alter table cura add foreign key (decko) references decko(sifra);
+alter table decko_zarucnica add foreign key (decko) references decko(sifra);
+alter table decko_zarucnica add foreign key (zarucnica) references zarucnica(sifra);
+
+
