a299a1a3e4209a2289c51b69340788cb8dc24a12
Petar Jagrić <83279909+petarjagric@users.noreply.github.com>
2021-05-24 12:52:10 +0200

    create table

---
+++ kolokvij_vjezba4.sql
@@ -0,0 +1,71 @@
+drop database if exists kolokvij_vjezba4;
+create database kolokvij_vjezba4;
+use kolokvij_vjezba4;
+
+create table ostavljen(
+	sifra int not null primary key auto_increment,
+	modelnaocala varchar(43),
+	introvertno bit,
+	kuna decimal(14,10)
+);
+
+create table punac(
+	sifra int not null primary key auto_increment,
+	treciputa datetime,
+	majica varchar(46),
+	jmbag char(11) not null,
+	novcica decimal(18,7) not null,
+	maraka decimal(12,6) not null,
+	ostaveljen int not null
+);
+
+create table mladic(
+	sifra int not null primary key auto_increment,
+	kuna decimal(15,9),
+	lipa decimal(18,5),
+	nausinca int,
+	stilfrizura varchar(49),
+	vesta varchar(34)
+);
+
+create table zena_mladic(
+	sifra int not null primary key auto_increment,
+	zena int,
+	mladic int
+);
+
+create table zena(
+	sifra int not null primary key auto_increment,
+	suknja varchar(39) not null,
+	lipa decimal(18,7),
+	prstena int not null
+);
+
+create table snasa(
+	sifra int not null primary key auto_increment,
+	introvertno bit,
+	treciputa datetime,
+	haljina varchar(44) not null,
+	zena int not null
+);
+
+create table becar(
+	sifra int not null primary key auto_increment,
+	novcica decimal(14,8),
+	kratkamajica varchar(48) not null,
+	bojaociju varchar(36) not null,
+	snasa int not null
+);
+
+create table prijatelj(
+	sifra int not null primary key auto_increment,
+	eura decimal(16,9),
+	prstena int not null,
+	gustoca decimal(16,5),
+	jmbag char(11) not null,
+	suknja varchar(47) not null,
+	becar int not null
+);
+
+
+
